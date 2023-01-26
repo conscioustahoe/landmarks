23104c50dd292dbd377b55caf585d6bc225d4549
conscioustahoe <ashutoshnarang80@gmail.com>
2023-01-26 17:27:51 +0530

    add map view

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by Ashutosh Narang on 26/01/23.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
